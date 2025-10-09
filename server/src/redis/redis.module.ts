45ed9225f08e49377c602b5b40aa178fb96217dc
NguyenManhHuy <68103157+huymannhhuy17800@users.noreply.github.com>
2025-10-09 14:04:19 +0700

    add ttl redis cache

--- server/src/redis/redis.module.ts
+++ server/src/redis/redis.module.ts
@@ -11,7 +11,7 @@ import { redisStore } from 'cache-manager-redis-yet';
             imports: [ConfigModule], // Config module for getting config from .env file
             useFactory: async (config : ConfigService) => {
                 const store = await redisStore({
-                    ttl : 3, // 30s
+                    ttl : 30 * 1000, // 30s
                     socket: {
                         host: config.get<string>('REDIS_URL'),
                         port: config.get<number>('REDIS_PORT')
